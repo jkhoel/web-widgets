c1a22a0db3f1793ac1652cdf5bdc64e52e2e4028
Jens Kristian Hoel <github@hoelweb.com>
2020-04-04 21:03:35 +0200

    chg: added CircularDial demo: ClockDial

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 /* eslint-disable react/jsx-filename-extension */
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 import CircularDial from './components/widgets/circular-dial';
 import DemoDial from './components/widgets/circular-dial/examples/DemoDial';
 import StatusDial from './components/widgets/circular-dial/examples/StatusDial';
+import ClockDial from './components/widgets/circular-dial/examples/ClockDial';
 
 // Main App
 function App() {
@@ -13,13 +13,10 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <CircularDial value={33} color="hotpink" />
-        <DemoDial step={1} />
         <DemoDial step={2} />
-        <DemoDial step={3} />
-        <DemoDial step={4} />
         <StatusDial />
+        <ClockDial />
       </header>
     </div>
   );
--- src/components/widgets/circular-dial/examples/ClockDial.jsx
+++ src/components/widgets/circular-dial/examples/ClockDial.jsx
@@ -1,1 +1,24 @@
-// Uses the Circular Dial to show the time, with seconds powering the dial guage
+import React from 'react';
+import moment from 'moment';
+
+import CircularDial from '../index';
+
+/**
+ * This component demos the CircularDial used as a clock, with the ring lighting up with seconds in a minute
+ */
+
+export default function ClockDial() {
+  const [value, setValue] = React.useState(moment().second());
+  const [label, setLabel] = React.useState(moment().format('HH:mm:ss'));
+
+  React.useEffect(() => {
+    const interval = setInterval(() => {
+      setLabel(moment().format('HH:mm:ss'));
+      setValue(moment().second());
+    }, 500);
+
+    return () => clearInterval(interval);
+  });
+
+  return <CircularDial value={value} maxValue={60} color="orange" label={label} labelOnly />;
+}
