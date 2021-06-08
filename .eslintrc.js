c64c604193862181bcf0ac40cd050a9e42760740
Jens Kristian Hoel <jens.kristian@hoelweb.com>
2021-06-08 12:33:16 +0200

    Switched to using Storybook.io

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,6 +9,7 @@ module.exports = {
   rules: {
     'react/jsx-one-expression-per-line': [0, { allow: 'single-child' }],
     'better-styled-components/sort-declarations-alphabetically': 2,
+    'react/jsx-props-no-spreading': 0,
     'prettier/prettier': [
       'error',
       {
