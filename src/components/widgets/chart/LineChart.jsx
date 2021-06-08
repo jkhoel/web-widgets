6a3022affedec4ba56e95c80d9b1afef283485d0
Jens Kristian Hoel <jens.kristian@hoelweb.com>
2021-06-08 20:23:18 +0200

    Added support for multiple data-series

--- src/components/widgets/chart/LineChart.jsx
+++ src/components/widgets/chart/LineChart.jsx
@@ -3,51 +3,43 @@ import PropTypes from 'prop-types';
 
 const STROKE = 1;
 
-// Axis Component
-const Axis = ({ points }) => (
-  <polyline fill="none" stroke="#ccc" strokeWidth=".5" points={points} />
-);
-
-Axis.propTypes = {
-  points: PropTypes.string.isRequired,
-};
-
 // Main LineChart Component
 const LineChart = ({
+  labels,
   data,
   width,
   height,
   horizontalGuides: numberOfHorizontalGuides,
   verticalGuides: numberOfVerticalGuides,
   precision,
+  colors,
 }) => {
   const FONT_SIZE = width / 50;
-  const maximumXFromData = Math.max(...data.map((e) => e.x));
-  const maximumYFromData = Math.max(...data.map((e) => e.y));
+  const maximumXFromData = Math.max(...data.map(({ values }) => values.map((v) => v.x)).flat());
+  const maximumYFromData = Math.max(...data.map(({ values }) => values.map((v) => v.y)).flat());
 
   const digits = parseFloat(maximumYFromData.toString()).toFixed(precision).length + 1;
 
   const padding = (FONT_SIZE + digits) * 3;
-  // const padding = 5;
   const chartWidth = width - padding * 2;
   const chartHeight = height - padding * 2;
 
-  // Axis start: (padding, height - padding) Axis end: (width - padding, height - padding)
+  // Axis Component
+  const Axis = ({ points }) => (
+    <polyline fill="none" stroke={colors.axis} strokeWidth=".5" points={points} />
+  );
+
+  Axis.propTypes = {
+    points: PropTypes.string.isRequired,
+  };
+
+  // Line for the X axis
   const XAxis = () => (
     <Axis points={`${padding},${height - padding} ${width - padding},${height - padding}`} />
   );
 
-  // Axis start: (padding, padding)   // Axis end: (padding, height - padding)
+  // Line for the X axis
   const YAxis = () => <Axis points={`${padding},${padding} ${padding},${height - padding}`} />;
-
-  // Iterate over each point in the data and create plot coordinates
-  const points = data
-    .map((dataPoint) => {
-      const x = (dataPoint.x / maximumXFromData) * chartWidth + padding;
-      const y = chartHeight - (dataPoint.y / maximumYFromData) * chartHeight + padding;
-      return `${x}, ${y}`;
-    })
-    .join(' ');
 
   // Create horizontal guides
   const HorizontalGuides = () => {
@@ -62,7 +54,7 @@ const LineChart = ({
         <polyline
           key={`horiz-guide-${index + 1}`}
           fill="none"
-          stroke="#ccc"
+          stroke={colors.guides}
           strokeWidth=".5"
           points={`${startX},${yCoordinate} ${endX},${yCoordinate}`}
         />
@@ -85,7 +77,7 @@ const LineChart = ({
         <polyline
           key={`vert-guide-${index + 1}`}
           fill="none"
-          stroke="#ccc"
+          stroke={colors.guides}
           strokeWidth=".5"
           points={`${xCoordinate},${startY} ${xCoordinate},${endY}`}
         />
@@ -93,25 +85,29 @@ const LineChart = ({
     });
   };
 
+  // Generate X-axis Labels
   const LabelsXAxis = () => {
+    // Find y position ("height")
     const y = height - padding + FONT_SIZE * 2;
 
-    return data.map((dataPoint) => {
-      const x = (dataPoint.x / maximumXFromData) * chartWidth + padding - FONT_SIZE / 2;
+    // Create labels
+    return labels.map((label, index) => {
+      const x = (index / maximumXFromData) * chartWidth + padding - FONT_SIZE / 2;
 
       return (
         <text
           x={x}
           y={y}
-          style={{ fill: '#ccc', fontSize: FONT_SIZE, fontFamily: 'Helvetica' }}
-          key={`xaxis-label-${dataPoint.x}`}
+          style={{ fill: colors.labels, fontSize: FONT_SIZE, fontFamily: 'Helvetica' }}
+          key={`x-label-${label}`}
         >
-          {dataPoint.label}
+          {label}
         </text>
       );
     });
   };
 
+  // Generate Y-axis labels
   const LabelsYAxis = () => {
     const PARTS = numberOfHorizontalGuides;
 
@@ -126,8 +122,8 @@ const LineChart = ({
         <text
           x={x}
           y={yCoordinate}
-          style={{ fill: '#ccc', fontSize: FONT_SIZE, fontFamily: 'Helvetica' }}
-          key={`yaxis-label-${index + 1}`}
+          style={{ fill: colors.labels, fontSize: FONT_SIZE, fontFamily: 'Helvetica' }}
+          key={`y-label-${index + 1}`}
         >
           {parseFloat(maximumYFromData * (index / PARTS)).toFixed(precision)}
         </text>
@@ -135,10 +131,37 @@ const LineChart = ({
     });
   };
 
-  // return <div>hello world</div>;
+  // Iterate over each point in the data and create plot coordinates
+  const points = data
+    .map((dataPoint) => {
+      const x = (dataPoint.x / maximumXFromData) * chartWidth + padding;
+      const y = chartHeight - (dataPoint.y / maximumYFromData) * chartHeight + padding;
+      return `${x}, ${y}`;
+    })
+    .join(' ');
+
+  // Create lines for each data-series
+  const chartSeries = data.map(({ legend, color, values }) => {
+    const coords = values
+      .map((dataPoint) => {
+        const x = (dataPoint.x / maximumXFromData) * chartWidth + padding;
+        const y = chartHeight - (dataPoint.y / maximumYFromData) * chartHeight + padding;
+        return `${x}, ${y}`;
+      })
+      .join(' ');
+
+    return { key: legend, color, coords };
+  });
 
   return (
     <svg viewBox={`0 0 ${width} ${height}`}>
+      <rect
+        x={padding}
+        y={padding}
+        width={chartWidth}
+        height={chartHeight}
+        fill={colors.background}
+      />
       <XAxis />
       <LabelsXAxis />
       <YAxis />
@@ -146,7 +169,11 @@ const LineChart = ({
       {numberOfVerticalGuides && <VerticalGuides />}
       <HorizontalGuides />
 
-      <polyline fill="none" stroke="#0074d9" strokeWidth={STROKE} points={points} />
+      {chartSeries.map(({ key, color, coords }) => (
+        <polyline key={key} fill="none" stroke={color} strokeWidth={STROKE} points={coords} />
+      ))}
+
+      <polyline fill="none" stroke={colors.line} strokeWidth={STROKE} points={points} />
     </svg>
   );
 };
@@ -157,13 +184,27 @@ LineChart.defaultProps = {
   horizontalGuides: 4,
   verticalGuides: null,
   precision: 2,
+  colors: {
+    labels: '#ccc',
+    axis: '#ccc',
+    guides: '#ccc',
+    line: 'hotpink',
+    background: '#eee',
+  },
 };
 
 LineChart.propTypes = {
+  labels: PropTypes.arrayOf(PropTypes.string).isRequired,
   data: PropTypes.arrayOf(
     PropTypes.shape({
-      value: PropTypes.number,
-      label: PropTypes.string,
+      legend: PropTypes.string,
+      color: PropTypes.string,
+      values: PropTypes.arrayOf(
+        PropTypes.shape({
+          x: PropTypes.number,
+          y: PropTypes.number,
+        }),
+      ),
     }),
   ).isRequired,
   height: PropTypes.number,
@@ -171,6 +212,15 @@ LineChart.propTypes = {
   horizontalGuides: PropTypes.number,
   verticalGuides: PropTypes.number,
   precision: PropTypes.number,
+  colors: PropTypes.objectOf(
+    PropTypes.shape({
+      labels: PropTypes.string,
+      axis: PropTypes.string,
+      guides: PropTypes.string,
+      line: PropTypes.string,
+      background: PropTypes.string,
+    }),
+  ),
 };
 
 export default LineChart;
--- src/stories/data-visualization/LineChart.stories.jsx
+++ src/stories/data-visualization/LineChart.stories.jsx
@@ -1,14 +1,45 @@
 import React from 'react';
 import LineChart from '../../components/widgets/chart/LineChart';
 
+// const data = [
+//   { label: 'SU', x: 0, y: 0 },
+//   { label: 'M', x: 1, y: 400 },
+//   { label: 'T', x: 2, y: 300 },
+//   { label: 'W', x: 3, y: 100 },
+//   { label: 'TH', x: 4, y: 400 },
+//   { label: 'F', x: 5, y: 500 },
+//   { label: 'SA', x: 6, y: 400 },
+// ];
+
+const labels = ['SU', 'M', 'T', 'W', 'TH', 'F', 'SA'];
+
 const data = [
-  { label: 'SU', x: 0, y: 0 },
-  { label: 'M', x: 1, y: 400 },
-  { label: 'T', x: 2, y: 300 },
-  { label: 'W', x: 3, y: 100 },
-  { label: 'TH', x: 4, y: 400 },
-  { label: 'F', x: 5, y: 500 },
-  { label: 'SA', x: 6, y: 400 },
+  {
+    legend: 'series1',
+    color: 'hotpink',
+    values: [
+      { x: 0, y: 0 },
+      { x: 1, y: 400 },
+      { x: 2, y: 300 },
+      { x: 3, y: 100 },
+      { x: 4, y: 400 },
+      { x: 5, y: 500 },
+      { x: 6, y: 400 },
+    ],
+  },
+  {
+    legend: 'series2',
+    color: 'chartreuse',
+    values: [
+      { x: 0, y: 100 },
+      { x: 1, y: 200 },
+      { x: 2, y: 100 },
+      { x: 3, y: 500 },
+      { x: 4, y: 400 },
+      { x: 5, y: 200 },
+      { x: 6, y: 300 },
+    ],
+  },
 ];
 
 export default {
@@ -16,6 +47,14 @@ export default {
   component: LineChart,
   argTypes: {
     data,
+    labels,
+    colors: {
+      labels: '#ccc',
+      axis: 'black',
+      guides: '#ccc',
+      line: 'hotpink',
+      background: '#eee',
+    },
   },
 };
 
@@ -24,4 +63,12 @@ const Template = (args) => <LineChart {...args} />;
 export const Default = Template.bind({});
 Default.args = {
   data,
+  labels,
+  colors: {
+    labels: '#ccc',
+    axis: 'black',
+    guides: '#ccc',
+    line: 'hotpink',
+    background: '#eee',
+  },
 };
